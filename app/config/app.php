7568d76707f3e66a39d5d60fdbfc0490cabd8aaa
Ruben Ponce <halodragon2008@gmail.com>
2026-08-11 22:36:29 -0600

    Support production base URL

--- app/config/app.php
+++ app/config/app.php
@@ -1,16 +1,27 @@
 <?php
-return [
+
+$host = $_SERVER["HTTP_HOST"] ?? "";
+$host = strtolower(explode(":", $host)[0]);
+
+$productionHosts = [
+    "diplomados.dftrdev.com",
+];
+
+$isProduction = in_array($host, $productionHosts, true);
+
+return [    
     /*
     |--------------------------------------------------------------------------
     | APP
     |--------------------------------------------------------------------------
     */
+
     "name" => "Gestión de diplomados",
     "session_name" => "DIPLOMADOS_SESSION",
     "proyect_name" => "diplomados",
     "version" => "1.0.0",
-    "base_url" => "/diplomados/",
-    "development" => true,
+    "base_url" => $isProduction ? "/" : "/diplomados/",
+    "development" => !$isProduction,
     "home" => "dashboard",
 
     /*
