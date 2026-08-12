a56472a08837220a939aab9765e86cd12f3d184a
Ruben Ponce <halodragon2008@gmail.com>
2026-08-11 21:36:05 -0600

    ejemplo de archivo de configuracion de base de datos añadida

---
+++ app/Config/database.example.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    "default" => [
+        "host" => "127.0.0.1",
+        "port" => 3306,
+        "database" => "",
+        "username" => "",
+        "password" => "",
+        "charset" => "utf8"
+    ]
+];
