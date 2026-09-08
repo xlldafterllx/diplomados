58d9c49c0890da7fd2cc875ba068aea9397d1eba
Ruben Ponce <ruben.ing.phz117@gmail.com>
2026-09-08 14:43:52 -0600

    fix para mostrar am/pm en el selector con un elemento datetime

--- assets/js/classes/DateHelper.js
+++ assets/js/classes/DateHelper.js
@@ -127,7 +127,7 @@ class DateHelper {
             element,
             {
                 enableTime: true,
-                time_24hr: true,
+                time_24hr: false,
 
                 altInput: true,
                 altInputClass: "form-control",
