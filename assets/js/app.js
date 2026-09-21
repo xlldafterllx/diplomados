383ef1db22c6c4486ab42012f51d484bb458ed88
Ruben Ponce <ruben.ing.phz117@gmail.com>
2026-09-21 12:03:05 -0600

    agregada función para tooltips

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,6 +6,11 @@ $(".form-control").each(function (e) {
     });
 });
 
+function initializeToolTips() {
+    const tooltipTriggerList = document.querySelectorAll(`[data-bs-toggle="tooltip"]`);
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+}
+
 function scrollToElement(element) {
     $("html, body").animate({
         scrollTop: element.$context.offset().top
